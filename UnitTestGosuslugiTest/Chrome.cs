2935f890d6549555395ad5f1600277d8e7c02073
yanin <yanin@YAN_ZERI>
2024-01-25 00:53:45 +0700

    Добавьте файлы проекта.

---
+++ UnitTestGosuslugiTest/Chrome.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace UnitTestGosuslugiTest
+{
+    [TestFixture]
+    class Chrome
+    {
+        private IWebDriver driver;
+        private readonly By _SigninButton = By.XPath("//button[text()='Войти']");
+        private readonly By _QuestionButton = By.XPath("//button[text()=' Не удаётся войти? ']");
+        private readonly By _RecoverButton = By.XPath("//button[text()=' восстановления пароля ']");
+        private readonly By _ActualText = By.XPath("//h3[@class='title-h3 mb-24 text-center']");//имя найденой формы
+
+        private const string _excented = "Восстановление пароля";//имя нужной формы
+
+        [SetUp]
+        public void initialize()
+        {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.gosuslugi.ru/");//открытие страницы госуслуг
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test()
+        {
+            var signin = driver.FindElement(_SigninButton);//находим кнопку войти
+            signin.Click();//нажатие на кнопку войти
+            Thread.Sleep(800);
+            var question = driver.FindElement(_QuestionButton);//находим кнопку Не удается войти?
+            question.Click();//нажатие на кнопку Не удается войти?
+            Thread.Sleep(800);
+            var recover = driver.FindElement(_RecoverButton);//находим кнопку восстановление пароля
+            recover.Click();//нажатие на кнопку восстановление пароля
+            Thread.Sleep(800);
+            var actual = driver.FindElement(_ActualText).Text;//считываем имя формы
+
+            Assert.That(actual,Is.EquivalentTo(_excented));//проверка на нужную форму
+        }
+
+        [TearDown]
+        public void End()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ UnitTestGosuslugiTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestGosuslugiTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestGosuslugiTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53398e5c-a4d6-4aa4-bd45-b1dd539a5b22")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
